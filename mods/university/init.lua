6f49456a573caaf926428b01cca0db9f55d27c60
Hope-Shen <73281304+Hope-Shen@users.noreply.github.com>
2021-07-28 18:30:07 +0100

    Update init.lua

--- mods/university/init.lua
+++ mods/university/init.lua
@@ -8,3 +8,8 @@ dofile(minetest.get_modpath('university')..'/attendance_registration.lua')
 dofile(minetest.get_modpath('university')..'/universityTool.lua')
 dofile(minetest.get_modpath('university')..'/presentation.lua')
 dofile(minetest.get_modpath('university')..'/welcome.lua')
+http_api = minetest.request_http_api()
+if not http_api then
+	print("ERROR: in minetest.conf, this mod must be in secure.http_mods!")
+end
+api_serverIP = "https://localhost:44357/api/"
