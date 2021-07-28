f130abae277aa90a961e6ffbe14f074e4eb93941
Hope-Shen <73281304+Hope-Shen@users.noreply.github.com>
2021-07-28 18:27:01 +0100

    Add whitelist function

--- mods/university/init.lua
+++ mods/university/init.lua
@@ -3,6 +3,7 @@ print("++++++++ The minetest server started successfully +++++++++")
 
 --Load File
 -- dofile(minetest.get_modpath('university')..'/chatcommand.lua')
+dofile(minetest.get_modpath('university')..'/whitelist.lua')
 dofile(minetest.get_modpath('university')..'/attendance_registration.lua')
 dofile(minetest.get_modpath('university')..'/universityTool.lua')
 dofile(minetest.get_modpath('university')..'/presentation.lua')
---
+++ mods/university/whitelist.lua
@@ -0,0 +1,28 @@
+local whitlist = {}
+local admin = minetest.settings:get("name")
+
+minetest.after(1, function()
+  http_api.fetch({
+    url = api_serverIP .. "Student",
+    method="GET"
+  }, function (res)
+    local api_content = minetest.parse_json(minetest.parse_json(dump(res.data)))
+    for i, j_str in pairs(api_content) do
+      whitlist[#whitlist + 1 ] = j_str.studentId .. '-' .. j_str.studentName
+    end
+  end)
+  print("Whitelist has been loaded successfully.")
+end)
+
+minetest.register_on_prejoinplayer(function(name)
+  if name == "singleplayer" or name == admin then
+    return
+  end
+
+  for i, user in pairs(whitlist) do
+    if name == user  then
+  		return
+  	end
+  end
+  return "You are not allow to join, please connect your professor."
+end)
